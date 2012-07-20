cca90bb2465e24603a82075c6386640b32225415
chirdeepshetty <chirdeep.shetty@gmail.com>
2012-07-20 16:16:31 +0530

    Added normalization to the offences

--- app/controllers/offence_report_controller.rb
+++ app/controllers/offence_report_controller.rb
@@ -9,10 +9,22 @@ class OffenceReportController < ApplicationController
   def offences
     video = Video.find(params[:id]) || not_found
     @offences = video.offences.order(:time).group(:time).count
+    logger.info @offences.inspect
+    normalize
   end
   
   private
   def not_found
     raise ActionController::RoutingError.new('Not Found')
+  end
+  
+  def normalize
+    @offences.keys.each do |time|
+      if @offences[time -1] then
+        @offences[time -1] += @offences[time]
+        @offences.delete(time)
+      end
+    end
+    
   end
 end
